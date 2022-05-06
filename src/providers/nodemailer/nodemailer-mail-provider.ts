4bc4b1b1245eeac1764257eb9af2c6e15229e344
Emilio Sanches <emilio.neto.32123@gmail.com>
2022-05-06 13:52:26 -0300

    Store mail credentials in dotenv

--- src/providers/nodemailer/nodemailer-mail-provider.ts
+++ src/providers/nodemailer/nodemailer-mail-provider.ts
@@ -1,22 +1,26 @@
 import { MailProvider, SendMailData } from "../mail-provider";
-import nodemailer from 'nodemailer';
-
-const transport = nodemailer.createTransport({
-  host: "smtp.mailtrap.io",
-  port: 2525,
-  auth: {
-    user: "4fd52760f57a00",
-    pass: "118ea99f899de9",
-  },
-});
+import nodemailer from "nodemailer";
 
 export class NodemailerMailProvider implements MailProvider {
+  private transport;
+
+  constructor() {
+    this.transport = nodemailer.createTransport({
+      host: "smtp.mailtrap.io",
+      port: 2525,
+      auth: {
+        user: process.env.MAILTRAP_USER,
+        pass: process.env.MAILTRAP_PASSWORD,
+      },
+    });
+  }
+
   async sendMail({ subject, body }: SendMailData) {
-    await transport.sendMail({
+    await this.transport.sendMail({
       from: "Equipe Feedget <feedback@feedget.com>",
       to: "Emilio Sanches <emiliosneto13@gmail.com>",
       subject,
-      html: body
+      html: body,
     });
   }
 }
